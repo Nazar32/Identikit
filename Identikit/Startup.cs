600acf9482d9e50caed82cdfe4a8d95a01c57a4d
Nazar32 <nazar3223@gmail.com>
2017-03-10 20:25:58 +0200

    Добавьте файлы проекта.

---
+++ Identikit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identikit.Startup))]
+namespace Identikit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
